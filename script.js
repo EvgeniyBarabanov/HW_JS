141a1579b74c875a465f5f9caec39b410eb25df6
EvgeniyBarabanov <evgeniy.barabanov.96@mail.ru>
2022-04-07 12:45:38 +0300

    exercise 11

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-/* document.write("<h2>Задание1</h2>")
+document.write("<h2>Задание1</h2>")
 
 {
     let arr = [1,2,3,4,5];
     for(let i = 0; i < arr.length;i++){
         document.write(arr[i])
     }
-} */
+}
 
 
-/* document.write("<h2>Задание2</h2>")
+document.write("<h2>Задание2</h2>")
 
 {
     let arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
@@ -17,10 +17,10 @@
          document.write(arr[i] + ' ');
         }
     }
-} */
+}
 
 
-/* document.write("<h2>Задание3</h2>")
+document.write("<h2>Задание3</h2>")
 
 {
     document.write("цикл for: ")
@@ -42,10 +42,10 @@
         i++;
     }
     document.write(arr1)
-} */
+}
 
 
-/* document.write("<h2>Задание4</h2>")
+document.write("<h2>Задание4</h2>")
 
 {
     let arr = ["10", "20", "30", "50", "235", "3000"]
@@ -55,10 +55,10 @@
             document.write(arr[i] + "<br>")
         }
     }
-} */
+}
 
 
-/* document.write("<h2>Задание5</h2>")
+document.write("<h2>Задание5</h2>")
 
 {
     let arr = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
@@ -69,11 +69,95 @@
             document.write(arr[i] + " ")
         }
     }
-} */
+}
 
 
 document.write("<h2>Задание6</h2>")
 
 {
-    
+    let arr = [1, 2, "три", ["четыре", 5], 6]
+    arr.push(76)
+    document.write("Последний элемент массива: " +  arr.length)
+}
+
+
+document.write("<h2>Задание7</h2>")
+
+{
+    let arr = [];
+    for(let i = 0; i <= 0; i++){
+        let num = Number(prompt("Введите число"));
+        i--;
+        if( num == ""){
+            break;
+        }else{
+            arr.push(num);
+        }
+    }
+    document.write("Полученный массив: " + arr + "<br>")
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i; j++){
+            if(arr[j] > arr[j + 1]){
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    document.write("Отсортированный массив: " + arr)
+}
+
+
+document.write("<h2>Задание8</h2>")
+
+{
+    let arr = [12, false, "Текст", 4, 2, -5, 0];
+    let arrReverse = arr.reverse();
+    document.write("Перевернутый массив: ");
+    let i = 0;
+    while(arrReverse.length > i){
+        document.write("<br>" + arrReverse[i]);
+        i++;
+    }
+}
+
+
+document.write("<h2>Задание9</h2>")
+
+{
+    let arr = [5,9,21,,,9,78,,,,6];
+    let number = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == undefined){
+            number++
+        }
+    }
+    document.write("Количество пустых элементов в массиве: " + number)
+}
+
+
+document.write("<h2>Задание10</h2>")
+
+{
+    let arr = [48,9,0,4,21,2,1,8,0,84,0,76,8,4,0,13,2];
+    let sum = 0;
+    for(let i = arr.indexOf(0); i < arr.lastIndexOf(0); i++){
+        sum+= arr[i];
+    }
+    document.write("сумма элементов массива между первы и последним 0 = " + sum);
+}
+
+
+document.write("<h2>Задание11</h2>")
+
+{
+    let h = 5;
+    for(let i = 1; i <= h; i++){
+        let str = "";
+        for(j = 0; j < i * 2 - 1; j++){
+            str+= "*";
+        }
+        console.log(str);
+        str = "";
+    }
 }
