3093e8fde409c5732129d60b7e2dabff74bc27a6
EvgeniyBarabanov <evgeniy.barabanov.96@mail.ru>
2022-06-07 11:14:43 +0300

    contactsApp

--- js/contacts/contacts.js
+++ js/contacts/contacts.js
@@ -1,5 +1,6 @@
 class User{
     #data;
+    
     constructor(data){
         this.#data = data;
     };
@@ -93,39 +94,43 @@ class DOM{
 
 }
 
-
-
 class ContactsApp extends Contacts{
     #inputNameElem;
     #inputEmailElem;
     #inputAddressElem;
     #inputPhoneElem;
     #contactsListElem;
+    #contactsFormElem;
+    #submitFormElem
 
-    onAdd = (event) =>{
-        if(event.code != 'Enter' || !event.ctrlKey) return;
+    onAdd = () =>{
 
         let contactsData = {};
 
         if(this.#inputNameElem.value.length > 0){
             contactsData.name = this.#inputNameElem.value;
         }
+        
         if(this.#inputEmailElem.value.length > 0){
             contactsData.email = this.#inputEmailElem.value;
         }
+
         if(this.#inputAddressElem.value.length > 0){
             contactsData.address = this.#inputAddressElem.value;
         }
+
         if(this.#inputPhoneElem.value.length > 0){
             contactsData.phone = this.#inputPhoneElem.value;
         }
         
+        if(contactsData.name || contactsData.email || contactsData.address || contactsData.phone) this.add(contactsData);
+            else return;
+
         this.#inputNameElem.value = "";
         this.#inputEmailElem.value = "";
         this.#inputAddressElem.value = "";
         this.#inputPhoneElem.value = "";
 
-        this.add(contactsData);
         this.update();
 
     }
@@ -141,16 +146,12 @@ class ContactsApp extends Contacts{
             ulElem.classList.add('user');
 
             const nameElem = DOM.create('li');
-            nameElem.classList.add('user__name');
 
             const emailElem = DOM.create('li');
-            emailElem.classList.add('user__email');
 
             const addressElem = DOM.create('li');
-            addressElem.classList.add('user__address');
 
             const phoneElem = DOM.create('li');
-            phoneElem.classList.add('user__phone');
 
             const btnEditElem = DOM.create('button')
             btnEditElem.classList.add('user__btn_edit');
@@ -160,9 +161,16 @@ class ContactsApp extends Contacts{
             btnRemoveElem.classList.add('user__btn_remove');
             btnRemoveElem.innerHTML = "X";
 
-            nameElem.innerHTML = user.name;
+            if(user.name == undefined || user.name == '') user.name = "-";
+             nameElem.innerHTML = user.name;
+
+            if(user.email == undefined || user.email == '') user.email = "-";
             emailElem.innerHTML = user.email;
+
+            if(user.address == undefined || user.email == '') user.address = "-";
             addressElem.innerHTML = user.address;
+
+            if(user.phone == undefined || user.phone == '') user.phone = "-";
             phoneElem.innerHTML = user.phone;
 
             ulElem.append(nameElem, emailElem, addressElem, phoneElem, btnEditElem, btnRemoveElem);
@@ -188,22 +196,47 @@ class ContactsApp extends Contacts{
 
         if(userFormEditElem) userFormEditElem.remove();
 
-        userFormEditElem = DOM.create('div');
+        userFormEditElem = DOM.create('form');
         userFormEditElem.classList.add('contact__form_edit');
+        userFormEditElem.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            let newData = {};
+
+            newData.name = inputNameElem.value;
+            newData.email = inputEmailElem.value;
+            newData.address = inputAddressElem.value;
+            newData.phone = inputPhoneElem.value;
+
+            userFormEditElem.remove();
+
+            if(newData.name || newData.email || newData.address || newData.phone) this.edit(id, newData);
+            else this.remove(id);
+            
+            this.update(); 
+            
+          }, true);
 
         const inputNameElem = DOM.create('input');
+        inputNameElem.setAttribute('name', 'name');
         inputNameElem.placeholder = "enter name";
 
         const inputEmailElem = DOM.create('input');
+        inputEmailElem.setAttribute('type','email');
+        inputEmailElem.setAttribute('name', 'email');
         inputEmailElem.placeholder = "enter email";
 
         const inputAddressElem = DOM.create('input');
+        inputAddressElem.setAttribute('name', 'address');
         inputAddressElem.placeholder = "enter address";
 
         const inputPhoneElem = DOM.create('input');
+        inputPhoneElem.setAttribute('name', 'phone');
+        inputPhoneElem.setAttribute('type','tel');
         inputPhoneElem.placeholder = "enter phone";
 
-        const btnSaveElem = DOM.create('button')
+        const btnSaveElem = DOM.create('button');
+        btnSaveElem.setAttribute('type', 'submit');
         btnSaveElem.classList.add('user__btn_save');
         btnSaveElem.innerHTML = "Save";
 
@@ -226,21 +259,6 @@ class ContactsApp extends Contacts{
         userFormEditElem.append(inputNameElem, inputEmailElem, inputAddressElem, inputPhoneElem, btnSaveElem)
         document.body.append(userFormEditElem);
         
-        btnSaveElem.addEventListener('click', () =>{
-            let newData = {};
-
-            newData.name = inputNameElem.value;
-            newData.email = inputEmailElem.value;
-            newData.address = inputAddressElem.value;
-            newData.phone = inputPhoneElem.value;
-
-            userFormEditElem.remove();
-
-            if(newData.name || newData.email || newData.address || newData.phone) this.edit(id, newData);
-            else this.remove(id);
-            
-            this.update() 
-        })
     }
 
     onRemove(id){
@@ -249,34 +267,47 @@ class ContactsApp extends Contacts{
     }
     
 
-    start(idElem){//стартовая точка интерфейса
+    start(idElem){
         if(!idElem) return;
 
         const appElem = DOM.search(`#${idElem}`);
         if(!appElem) return;
 
-        const contactsFormElem = DOM.create('div');
-        contactsFormElem.classList.add('contacts__form');
+        this.#contactsFormElem = DOM.create('form');
+        this.#contactsFormElem.classList.add('contacts__form');
+        this.#contactsFormElem.addEventListener("submit", (event) => {
+            event.preventDefault();
+            this.onAdd();
+          }, true);
         
         this.#contactsListElem = DOM.create('div');
         this.#contactsListElem.classList.add('contacts__list');
 
         this.#inputNameElem = DOM.create('input');
+        this.#inputNameElem.setAttribute('name', 'name');
         this.#inputNameElem.placeholder = "enter name";
 
         this.#inputEmailElem = DOM.create('input');
+        this.#inputEmailElem.setAttribute('type','email');
+        this.#inputEmailElem.setAttribute('name', 'email');
         this.#inputEmailElem.placeholder = "enter email";
 
         this.#inputAddressElem = DOM.create('input');
+        this.#inputAddressElem.setAttribute('name', 'address');
         this.#inputAddressElem.placeholder = "enter address";
 
         this.#inputPhoneElem = DOM.create('input');
+        this.#inputPhoneElem.setAttribute('name', 'phone');
+        this.#inputPhoneElem.setAttribute('type','tel');
         this.#inputPhoneElem.placeholder = "enter phone";
 
-        contactsFormElem.append(this.#inputNameElem, this.#inputEmailElem, this.#inputAddressElem, this.#inputPhoneElem);
-        appElem.append(contactsFormElem, this.#contactsListElem);
+        this.#submitFormElem = DOM.create('input');
+        this.#submitFormElem.setAttribute('type', 'submit');
+        this.#submitFormElem.value = "Добавить";
 
-        contactsFormElem.addEventListener('keyup', this.onAdd)
+        this.#contactsFormElem.append(this.#inputNameElem, this.#inputEmailElem, this.#inputAddressElem, this.#inputPhoneElem, this.#submitFormElem);
+        appElem.append(this.#contactsFormElem, this.#contactsListElem);
+
     };
     
 }
